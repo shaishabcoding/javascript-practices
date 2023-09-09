cde1b90d0c016861090abeb4c4073babf8ec7cb6
Shandra Chandra Shil <shaishabchandrashil@gmail.com>
2023-09-09 08:18:31 +0600

    done

---
+++ basicCalculator.js
@@ -0,0 +1,13 @@
+const sum = 3 + 2; // 5
+const sub = 3 - 2; // 1
+const mul = 3 * 2; // 6
+const div = 3 / 2; // 1.5
+const mod = 3 % 2; // 1 (remainder of division)
+const exp = 3 ** 2; // 9 (exponentiation)
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(mod);
+console.log(exp);
